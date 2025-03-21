1b02f4da70610733b3edbe11248e9850f8a37b69
NguyenVanTrung1910 <165651932+NguyenVanTrung1910@users.noreply.github.com>
2025-03-21 16:43:45 +0700

    Delete Vite directory

--- Vite/src/type/card-type.ts
+++
@@ -1,6 +0,0 @@
-export type TCardShadow = null | 'none' | 'sm' | 'md' | 'lg' | '3d';
-
-//export type TCardBorderSize = null | 0 | 1 | 2 | 3 | 4 | 5;
-export type TCardBorderSize =  1 ;
-
-export type TCardSize = 'sm' | null | 'lg';
